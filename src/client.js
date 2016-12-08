58e622e00e2575ecb5fd3fbace34b1a63b10e78c
thomasdigby <thomasdigby1@gmail.com>
2016-12-08 14:43:26 +0000

    Fixed client React render

--- src/client.js
+++ src/client.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, browserHistory } from 'react-router'
+import AsyncProps from 'async-props'
 
+import DefaultRoutes from './default-routes'
 import config from 'tapestry.js'
 
-const Post = config.components.Post
-
 render(
-  <Post />,
+  <Router
+    history={browserHistory}
+    render={props => <AsyncProps {...props} />}
+    routes={DefaultRoutes(config.components)}>
+  </Router>,
   document.getElementById('root')
 )
--- src/default-html.js
+++ src/default-html.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export default ({ markup }) =>
+export default ({ markup, asyncProps }) =>
   <html lang="en">
     <body>
       <div
         id="root"
         style={{ width: '100%' }}
         dangerouslySetInnerHTML={{ __html: markup }} />
+      <script dangerouslySetInnerHTML={{ __html: `window.__ASYNC_PROPS__ = ${JSON.stringify(asyncProps.propsArray)}` }} />
       <script src="/public/bundle.js" />
     </body>
   </html>
