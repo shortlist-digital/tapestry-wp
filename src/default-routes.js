67342aee3900999876b2f39e383a485725aa63ef
thomasdigby <thomasdigby1@gmail.com>
2016-12-08 16:57:38 +0000

    Added PageLoader

--- src/default-routes.js
+++ src/default-routes.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import PostLoader from './loader-post'
+import PageLoader from './loader-page'
 
-export default ({ Base, Post }) =>
+export default ({ Base, Post, Page }) =>
   <Route component={Base}>
+    <Route path='about/:slug' component={PageLoader} page={Page} />
     <Route path=':category/:slug/:id' component={PostLoader} post={Post} />
     <Route path=':category/:subcategory/:slug/:id' component={PostLoader} post={Post} />
   </Route>
---
+++ src/loader-page.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import AsyncProps from 'async-props'
+import fetch from 'isomorphic-fetch'
+
+export default class Loader extends Component {
+
+  static loadProps({params, loadContext}, cb) {
+    // LoadContext is basicaly an object we can pass around
+    // the sever with our components and some baseUrl on it
+    return fetch(`${loadContext.siteUrl}/wp-json/wp/v2/pages?filter[name]=${params.slug}`)
+      .then(response => response.json())
+      .then(data => cb(null, { data: data[0] }))
+      .catch(error => cb(error))
+  }
+
+  render () {
+    const Page = this.props.route.page
+    return <Page {...this.props.data} />
+  }
+}
