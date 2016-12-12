ce923fd9d1ac594de71ad34a95b86f4c5a951547
thomasdigby <thomasdigby1@gmail.com>
2016-12-12 11:39:02 +0000

    Added html attributes to default-html

--- src/default-html.js
+++ src/default-html.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import './default-style'
 
-export default ({ markup, head, asyncProps }) =>
-  <html lang="en">
-    <head>
-      {head.title.toComponent()}
-      {head.base.toComponent()}
-      {head.meta.toComponent()}
-      {head.link.toComponent()}
-      {head.script.toComponent()}
-      <style dangerouslySetInnerHTML={{ __html: markup.css }} />
-      <link rel="shortcut icon" href="http://shortlist.studio/public/favicon.ico" />
-    </head>
-    <body>
-      <div id="root" dangerouslySetInnerHTML={{ __html: markup.html }} />
-      <script dangerouslySetInnerHTML={{ __html: `window.__ASYNC_PROPS__ = ${JSON.stringify(asyncProps.propsArray)}` }} />
-      <script src="/public/bundle.js" />
-    </body>
-  </html>
+export default ({ markup, head, asyncProps }) => {
+  const attr = head.htmlAttributes.toComponent()
+  return (
+    <html {...attr}>
+      <head>
+        {head.title.toComponent()}
+        {head.base.toComponent()}
+        {head.meta.toComponent()}
+        {head.link.toComponent()}
+        {head.script.toComponent()}
+        <style dangerouslySetInnerHTML={{ __html: markup.css }} />
+      </head>
+      <body>
+        <div id="root" dangerouslySetInnerHTML={{ __html: markup.html }} />
+        <script dangerouslySetInnerHTML={{ __html: `window.__ASYNC_PROPS__ = ${JSON.stringify(asyncProps.propsArray)}` }} />
+        <script src="/public/bundle.js" />
+      </body>
+    </html>
+  )
+}
