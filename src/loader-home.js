108fa76c5e48c2c678e13cb758a8c9d29f77c259
thomasdigby <thomasdigby1@gmail.com>
2017-01-04 15:01:14 +0000

    Added handler for frontPage option, accepts id or slug

--- src/loader-home.js
+++ src/loader-home.js
@@ -5,11 +5,25 @@ import fetch from 'isomorphic-fetch'
 export default class Loader extends Component {
 
   static loadProps({ params, loadContext }, cb) {
+
+    const baseUrl = `${loadContext.siteUrl}/wp-json/wp/v2`
+
+    let url = `pages?filter[name]=home`
+    if (parseInt(loadContext.frontPage))
+      url = `pages/${loadContext.frontPage}`
+    else if (typeof loadContext.frontPage === 'string')
+      url = `pages?filter[name]=${loadContext.frontPage}`
+
     // LoadContext is basicaly an object we can pass around
     // the sever with our components and some baseUrl on it
-    return fetch(`${loadContext.siteUrl}/wp-json/wp/v2/posts?_embed`)
-      .then(response => response.json())
-      .then(data => cb(null, { data }))
+    return fetch(`${baseUrl}/${url}`)
+      .then(resp => resp.json())
+      .then(resp => {
+        const data = ('0' in resp) || resp instanceof Array ?
+        { data: resp[0] } :
+        { resp }
+        return cb(null, data)
+      })
       .catch(error => cb(error))
   }
 
