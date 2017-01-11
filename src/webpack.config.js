9ccec7f32447f499786bdc6fddc309baf4ba744a
Jon Sherrard <jon@andthats.it>
2017-01-11 16:41:34 +0000

    compile server command

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,42 +1,57 @@
 import path from 'path'
+import fs from 'fs'
 import webpackCleanPlugin from 'clean-webpack-plugin'
 
-export default (context, isTree = false) => ({
-  resolve: {
-    modulesDirectories: [context, `${context}/node_modules`]
-  },
-  entry: isTree ? `${context}/tapestry.js` : 'tapestry-wp/dist/client.js',
-  output: {
-    path: path.resolve(context, isTree ? 'dist' : 'public'),
-    filename: isTree ? 'tree.js' : 'bundle.js',
-    libraryTarget: isTree ? 'commonjs2' : 'var'
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react']
+export default (context, isServer = false) => {
+  const config = {
+    resolve: {
+      modulesDirectories: [context, `${context}/node_modules`]
+    },
+    entry: isServer ? 'tapestry-wp/dist/boot.js' : 'tapestry-wp/src/client.js',
+    output: {
+      path: path.resolve(context, isServer ? 'server' : 'public'),
+      filename: isServer ? 'server.js' : 'bundle.js',
+      libraryTarget: isServer ? 'commonjs2' : 'var'
+    },
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }]
+    },
+    plugins: [
+      new webpackCleanPlugin(['public'], { root: context, verbose: false })
+      // new webpack.optimize.UglifyJsPlugin(),
+      // new webpack.optimize.OccurrenceOrderPlugin(),
+      // new webpack.optimize.DedupePlugin(),
+      // new webpack.DefinePlugin({
+      //   'process.env': {
+      //     'NODE_ENV': JSON.stringify('production')
+      //   }
+      // })
+    ]
+  }
+  if (isServer) {
+
+      config.node = {
+        fs: "empty"
       }
-    }]
-  },
-  externals: isTree ? [
-    {
-      'isomorphic-fetch': {
-        commonjs2: 'isomorphic-fetch'
-      }
-    }
-  ] : [],
-  plugins: [
-    new webpackCleanPlugin(['public'], { root: context, verbose: false })
-    // new webpack.optimize.UglifyJsPlugin(),
-    // new webpack.optimize.OccurrenceOrderPlugin(),
-    // new webpack.optimize.DedupePlugin(),
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     'NODE_ENV': JSON.stringify('production')
-    //   }
-    // })
-  ]
-})
+
+      config.externals = fs.readdirSync('node_modules')
+        .filter((x) => {
+          return ['.bin'].indexOf(x) === -1
+        })
+        .reduce((accumulator, mod) => {
+          accumulator[mod] = `commonjs2 ${mod}`
+          return accumulator
+        }, {})
+      config.externals['glamor/server'] = 'commonjs2 glamor/server'
+  }
+
+  return config
+
+}
