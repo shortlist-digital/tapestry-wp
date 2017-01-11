2d891ab979df79357ffed1cf96e8f3a8519b18ad
thomasdigby <thomasdigby1@gmail.com>
2017-01-11 10:45:49 +0000

    Initial rewrite of Webpack config, introducing production plugins

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,42 +1,69 @@
 import path from 'path'
-import webpackCleanPlugin from 'clean-webpack-plugin'
+import webpack from 'webpack'
+import CleanPlugin from 'clean-webpack-plugin'
 
-export default (context, isTree = false) => ({
-  resolve: {
-    modulesDirectories: [context, `${context}/node_modules`]
-  },
-  entry: isTree ? `${context}/tapestry.js` : 'tapestry-wp/dist/client.js',
-  output: {
-    path: path.resolve(context, isTree ? 'dist' : 'public'),
-    filename: isTree ? 'tree.js' : 'bundle.js',
-    libraryTarget: isTree ? 'commonjs2' : 'var'
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react']
-      }
-    }]
-  },
-  externals: isTree ? [
-    {
+const env = process.env.NODE_ENV
+
+
+export default (ctx, isTree = false) => {
+
+  const config = {
+    resolve: {
+      modules: [ctx, `${ctx}/node_modules`]
+    },
+    plugins: [
+      new CleanPlugin(['public'], {
+        root: ctx,
+        verbose: false
+      })
+    ],
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react']
+        }
+      }]
+    }
+  }
+
+  if (isTree) {
+    config.entry = `${ctx}/tapestry.js`
+    config.output = {
+      path: path.resolve(ctx, 'dist'),
+      filename: 'tree.js',
+      libraryTarget: 'commonjs2'
+    }
+    config.externals = [{
       'isomorphic-fetch': {
         commonjs2: 'isomorphic-fetch'
       }
+    }]
+  } else {
+    config.entry = 'tapestry-wp/dist/client.js'
+    config.output = {
+      path: path.resolve(ctx, 'public'),
+      filename: 'bundle.js'
     }
-  ] : [],
-  plugins: [
-    new webpackCleanPlugin(['public'], { root: context, verbose: false })
-    // new webpack.optimize.UglifyJsPlugin(),
-    // new webpack.optimize.OccurrenceOrderPlugin(),
-    // new webpack.optimize.DedupePlugin(),
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     'NODE_ENV': JSON.stringify('production')
-    //   }
-    // })
-  ]
-})
+  }
+
+  if (env === 'production') {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+       'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      }),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true
+      })
+    )
+  }
+
+  return config
+}
