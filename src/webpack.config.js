1e37a332e1c9a955250028cc9a02228fcb3b163b
thomasdigby <thomasdigby1@gmail.com>
2017-01-20 10:51:21 +0000

    Removed tree config

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -6,9 +6,14 @@ import CleanPlugin from 'clean-webpack-plugin'
 const env = process.env.NODE_ENV
 
 
-export default (ctx, isTree = false) => {
+export default (ctx) => {
 
   const config = {
+    entry: 'tapestry-wp/dist/client.js',
+    output: {
+      path: path.resolve(ctx, 'public'),
+      filename: 'bundle.js'
+    },
     resolve: {
       modules: [ctx, `${ctx}/node_modules`]
     },
@@ -17,9 +22,7 @@ export default (ctx, isTree = false) => {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        options: {
-          presets: ['es2015', 'react']
-        }
+        options: { presets: ['es2015', 'react'] }
       }]
     },
     plugins: [
@@ -30,46 +33,24 @@ export default (ctx, isTree = false) => {
     ]
   }
 
-  if (isTree) {
-    config.entry = `${ctx}/tapestry.js`
-    config.output = {
-      path: path.resolve(ctx, 'dist'),
-      filename: 'tree.js',
-      libraryTarget: 'commonjs2'
-    }
-    config.externals = [{
-      'isomorphic-fetch': {
-        commonjs2: 'isomorphic-fetch'
-      }
-    }]
-  } else {
-    config.entry = 'tapestry-wp/dist/client.js'
-    config.output = {
-      path: path.resolve(ctx, 'public'),
-      filename: 'bundle.js'
-    }
-  }
-
   if (env === 'production') {
     config.plugins.push(
       new webpack.DefinePlugin({
        'process.env.NODE_ENV': JSON.stringify('production')
       }),
       new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
+        compress: { warnings: false }
       }),
       new webpack.LoaderOptionsPlugin({
         minimize: true
       }),
       function () {
-        this.plugin('done', function (stats) {
+        this.plugin('done', stats =>
           fs.writeFileSync(
             path.resolve(ctx, 'stats.json'),
             JSON.stringify(stats.toJson())
           )
-        })
+        )
       }
     )
   }
