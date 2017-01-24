078d683f87c6142326124cdf6c037d585aacc51a
thomasdigby <thomasdigby1@gmail.com>
2017-01-24 17:17:03 +0000

    Removed comments

--- src/build.js
+++ src/build.js
@@ -4,15 +4,9 @@ import config from './webpack.config'
 export default class Build {
 
   constructor (context) {
-    this.compiler = new webpack(config(context), (err, stats) => {
-      console.log(stats.toString({colors: true}))
-      const jsonStats = stats.toJson()
-      // if (err)
-      //   console.error('Build Client Errors: ', err)
-      // if (jsonStats.errors.length > 0)
-      //   console.error('Build Client jsonStats Errors: ', jsonStats.errors)
-      // if (jsonStats.warnings.length > 0)
-      //   console.warn('Build client jsonStats Warnings: ', jsonStats.warnings)
-    })
+    this.compiler = new webpack(
+      config(context),
+      (err, stats) => console.log(stats.toString({ colors: true }))
+    )
   }
 }
