bf1ec8b5fa39b87deb32560b3ae3885642f64d79
thomasdigby <thomasdigby1@gmail.com>
2017-01-30 16:01:20 +0000

    Removed Base component

--- bootstrap/tapestry.config.js
+++ bootstrap/tapestry.config.js
@@ -1,10 +1,7 @@
-import Base from './components/base'
 import Page from './components/page'
 import Post from './components/post'
 
 export default {
-  components: {
-    Base, Page, Post
-  },
+  components: { Page, Post },
   siteUrl: 'http://shortliststudio.foundry.press'
 }
