3662b221653512aeeb05cf0c670cd5cafc594dda
thomasdigby <thomasdigby1@gmail.com>
2017-01-30 13:45:58 +0000

    All fs functions now use the sync version, initial defaults collected into bootstrap folder

---
+++ bootstrap/tapestry.config.js
@@ -0,0 +1,10 @@
+import Base from './components/base'
+import Page from './components/page'
+import Post from './components/post'
+
+export default {
+  components: {
+    Base, Page, Post
+  },
+  siteUrl: 'http://shortliststudio.foundry.press'
+}
