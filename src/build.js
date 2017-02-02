b28a28fd3999ff0d8ab21e719c1e491bf78613f4
thomasdigby <thomasdigby1@gmail.com>
2017-02-02 09:32:49 +0000

    Clearing require cache on request to pull in latest project changes, updated webpack build to watch

--- src/build.js
+++ src/build.js
@@ -3,20 +3,45 @@ import config from './webpack.config'
 import { success, error } from './logger'
 import bytes from 'pretty-bytes'
 
+let devNotified = false
+
 export default class Build {
 
   constructor (opts) {
-    // bundle client scripts
-    new webpack(config(opts), (err, stats) => {
-      // handle error
-      if (err)
-        error(err)
-      // log output
-      const output = stats.toJson()
-      success(`Client built: ${bytes(output.assets[0].size)}`)
-      // run callback
-      if (typeof opts.onComplete === 'function')
-        opts.onComplete(output)
-    })
+
+    this.opts = opts
+    this.compiler = webpack(config(opts))
+
+    if (opts.env !== 'development') {
+      this.compiler.run((err, stats) => this.run(err, stats))
+    } else {
+      this.compiler.watch({}, this.watch.bind(this))
+    }
+  }
+
+  run (err, stats) {
+    // handle error
+    if (err)
+      error(err)
+    // log complete
+    this.complete(stats)
+  }
+  watch (err, stats) {
+    // handle error
+    if (err)
+      error(err)
+    // log complete
+    if (!devNotified) {
+      this.complete(stats)
+      devNotified = true
+    }
+  }
+  complete (stats) {
+    // log output
+    const output = stats.toJson()
+    success(`Client built: ${bytes(output.assets[0].size)}`)
+    // run callback
+    if (typeof this.opts.onComplete === 'function')
+      this.opts.onComplete()
   }
 }
