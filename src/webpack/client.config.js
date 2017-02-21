ff10f32e5d6690b768366ef261e31f8c1ac9a5f5
thomasdigby <thomasdigby1@gmail.com>
2017-02-21 17:52:17 +0000

    Documenting config options

--- src/webpack/client.config.js
+++ src/webpack/client.config.js
@@ -1,34 +1,41 @@
-import fs from 'fs'
 import path from 'path'
 import webpack from 'webpack'
 import CleanPlugin from 'clean-webpack-plugin'
 import AssetsPlugin from 'assets-webpack-plugin'
+import StatsPlugin from 'stats-webpack-plugin'
 import rules from './rules'
 
+// exporting function to allow process.cwd() and environment to get passed through
 export default ({ cwd, env }) => {
 
   const config = {
+    // target the browser as runtime
     target: 'web',
     entry: {
       bundle: '../src/client/webpack.entry.js'
     },
+    // output bundle to _scripts, no caching required in dev mode so bundle.js is sufficient
     output: {
       path: path.resolve(cwd, '_scripts'),
       filename: '[name].js',
       publicPath: `/_scripts/`
     },
+    // when tapestry config is called from webpack.entry.js, resolve from the root of cwd
     resolve: {
       alias: {
         'tapestry.config.js': path.resolve(cwd, 'tapestry.config.js')
       }
     },
+    // share module rules with server config
     module: rules,
     plugins: [
+      // output public path data for each bundle
       new AssetsPlugin({
         filename: 'assets.json',
         path: path.resolve(cwd, '.tapestry'),
         prettyPrint: true
       }),
+      // remove output directory before saving new bundle
       new CleanPlugin(['_scripts'], {
         root: cwd,
         verbose: false
@@ -36,8 +43,11 @@ export default ({ cwd, env }) => {
     ]
   }
 
+  // production specific config
   if (env === 'production') {
+    // expose chunkhash naming for long-life caching
     config.output.filename = '[name].[chunkhash].js'
+    // non-changing vendor packages to combine in a vendor bundle
     config.entry.vendor = [
       'react',
       'react-dom',
@@ -46,29 +56,26 @@ export default ({ cwd, env }) => {
       'async-props'
     ]
     config.plugins.push(
+      // production flag for React/others to minify correctly
       new webpack.DefinePlugin({
        'process.env.NODE_ENV': JSON.stringify('production')
       }),
+      // output common chunks into a vendor bundle
       new webpack.optimize.CommonsChunkPlugin({
         name: 'vendor',
         minChunks: Infinity,
         filename: 'vendor.[chunkhash].js'
       }),
+      //
       new webpack.LoaderOptionsPlugin({
         minimize: true,
         debug: false
       }),
-      function () {
-        this.plugin('done', stats => {
-          const jsonStats = stats.toJson({
-            chunkModules: true
-          })
-          return fs.writeFileSync(
-            path.resolve(cwd, '.tapestry', 'stats.json'),
-            JSON.stringify(jsonStats)
-          )
-        })
-      },
+      // output chunk stats
+      new StatsPlugin(path.resolve(cwd, '.tapestry', 'stats.json'), {
+        chunkModules: true
+      }),
+      // minify/optimize output bundle, screw_ie8 a bunch
       new webpack.optimize.UglifyJsPlugin({
         comments: false,
         compress: {
--- src/webpack/server.config.js
+++ src/webpack/server.config.js
@@ -1,23 +1,27 @@
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
-
 const rules = require('./rules')
 const cwd = process.cwd()
 
-
 // module.exports to enable CLI usage
 module.exports = {
+  // target node as runtime
   target: 'node',
+  // enable webpack node polyfill for __dirname
   node: {
-    __dirname: true,
-    __filename: true
+    __dirname: true
   },
-  entry: path.resolve(cwd, 'src', 'index.js'),
+  entry: {
+    bundle: path.resolve(cwd, 'src', 'index.js')
+  },
+  // output bundle to dist dir, commonjs2 exports the bundle as module.exports
   output: {
     path: path.resolve(cwd, 'dist'),
-    filename: 'server.bundle.js',
+    filename: 'server.[name].js',
     libraryTarget: 'commonjs2'
   },
+  // share module rules with client config
   module: rules,
-  externals: [nodeExternals(), 'fs']
+  // ignore any node_modules packages, rely on native node require
+  externals: [nodeExternals()]
 }
