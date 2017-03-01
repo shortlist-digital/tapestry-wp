e9bfdcced9ff912860fb7cb6a2fdff8249845bfd
thomasdigby <thomasdigby1@gmail.com>
2017-03-01 21:34:15 +0000

    Added object-rest-spread

--- src/index.js
+++ src/index.js
@@ -11,17 +11,16 @@ import Client from './client'
 export const server = (options, devOptions) => new Server(options, devOptions)
 // creates the client bundle
 export const client = (options) => new Client(options)
-// create client bundle and boot server on callback, avoids the server booting without the client ready. Object.assign() used instead of spread operator as we're only supporting es2015 (currently)
-export const boot = (options) => new Client(
-  Object.assign({
-    onComplete: () => {
-      // get bundle data and pass through to server
-      const assetsPath = path.resolve(options.cwd, '.tapestry', 'assets.json')
-      const assets = fs.readJsonSync(assetsPath)
-      return new Server(Object.assign({}, options, { assets }))
-    }
-  }, options)
-)
+// create client bundle and boot server on callback, avoids the server booting without the client ready.
+export const boot = (options) => new Client({
+  onComplete: () => {
+    // get bundle data and pass through to server
+    const assetsPath = path.resolve(options.cwd, '.tapestry', 'assets.json')
+    const assets = fs.readJsonSync(assetsPath)
+    return new Server({ ...options, assets })
+  },
+  ...options
+})
 
 // allows default import
 // import tapestry from 'tapestry-wp'
