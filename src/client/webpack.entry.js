ca205eb026d5311099a6a491c8f65d80d70c9b88
thomasdigby <thomasdigby1@gmail.com>
2017-03-04 19:44:33 +0000

    Expose getComponent method on route, requires router match on client

--- src/client/webpack.entry.js
+++ src/client/webpack.entry.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, browserHistory } from 'react-router'
+import { Router, match, browserHistory } from 'react-router'
 import AsyncProps from 'async-props'
 import 'location-origin'
-
 import DefaultRoutes from '../shared/default-routes'
 import config from 'tapestry.config.js'
 
-let onUpdateMethod = () => {}
-if (typeof config.onPageUpdate === 'function') {
-  onUpdateMethod = config.onPageUpdate
-}
+// methods in use on the <Router />
+const onUpdate = () =>
+  typeof config.onPageUpdate === 'function' && config.onPageUpdate
 const renderAsyncProps = props =>
   <AsyncProps loadContext={config} {...props} />
 
-render(
-  <Router
-    onUpdate={onUpdateMethod}
-    history={browserHistory}
-    render={renderAsyncProps}
-    routes={DefaultRoutes(config)}>
-  </Router>,
-  document.getElementById('root')
+// define routes/history for react-router
+const routes = DefaultRoutes(config)
+const history = browserHistory
+const targetNode = document.getElementById('root')
+
+// run a router match (not sure why this is necessary)
+match({ routes, history }, (error, redirectLocation, renderProps) =>
+  render(
+    <Router
+      onUpdate={onUpdate}
+      render={renderAsyncProps}
+      routes={routes}
+      {...renderProps}
+    />,
+    targetNode
+  )
 )
--- src/shared/default-routes.js
+++ src/shared/default-routes.js
@@ -22,41 +22,53 @@ const DefaultRoutes = ({
   } = components
   const Error = components.Error || MissingView
 
-  // return Route with component directly
-  const renderStaticRoute = (route, i) =>
-    <Route
-      component={route.component}
-      key={i}
-      path={route.path} />
+  // return static Route with component directly
+  const renderStaticRoute = (route, i) => {
+    // on 'route' event:
+    // getComponent will fetch the component async
+    // component will read the component from the bundle
+    const component = route.getComponent ?
+      { getComponent: (loc, cb) => route.getComponent(loc, cb) } :
+      { component: route.component }
+    // return static route with async or bundled component
+    return (
+      <Route
+        key={i}
+        path={route.path}
+        {...component} />
+    )
+  }
 
-  return (<div>
-    {
-      routes.map(renderStaticRoute)
-    }
-    <Route
-      path='/'
-      component={FrontPageLoader}
-      tag={FrontPage}
-      fallback={Error} />
-    <Route
-      path='about/:slug'
-      component={PageLoader}
-      tag={Page}
-      fallback={Error} />
-    <Route
-      path=':category(/:subcategory)'
-      component={CategoriesLoader}
-      tag={Category}
-      fallback={Error} />
-    <Route
-      path=':category(/:subcategory)/:slug/:id'
-      component={PostLoader}
-      tag={Post}
-      fallback={Error} />
-    <Route
-      path='*'
-      component={Error} />
-  </div>)
+  return (
+    <div>
+      {
+        routes.map(renderStaticRoute)
+      }
+      <Route
+        path='/'
+        component={FrontPageLoader}
+        tag={FrontPage}
+        fallback={Error} />
+      <Route
+        path='about/:slug'
+        component={PageLoader}
+        tag={Page}
+        fallback={Error} />
+      <Route
+        path=':category(/:subcategory)'
+        component={CategoriesLoader}
+        tag={Category}
+        fallback={Error} />
+      <Route
+        path=':category(/:subcategory)/:slug/:id'
+        component={PostLoader}
+        tag={Post}
+        fallback={Error} />
+      <Route
+        path='*'
+        component={Error} />
+    </div>
+  )
 }
 
 DefaultRoutes.propTypes = {
