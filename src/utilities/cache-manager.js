854b8f78e4db50c7d8a3c1dd710cd349d35cae84
Jon Sherrard <jon@andthats.it>
2017-03-05 14:23:33 +0000

    Default to max age 0 unless on production

--- src/utilities/cache-manager.js
+++ src/utilities/cache-manager.js
@@ -4,15 +4,16 @@ const internalCaches = []
 
 export default class CacheManager {
 
-  static createCache(name) {
+  static createCache (name) {
     internalCaches[name] = LRU({
       max: 100,
-      maxAge: process.env.CACHE_MAX_AGE || 1000*60*2
+      maxAge: (process.env.NODE_ENV === 'production') ?
+        (process.env.CACHE_MAX_AGE || 1000*60*2) : 0
     })
     return internalCaches[name]
   }
 
-  static clearAll() {
+  static clearAll () {
     internalCaches.forEach(cache =>
       cache.reset()
     )
