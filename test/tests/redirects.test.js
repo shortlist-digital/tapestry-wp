57a99e5402883b3934093e833e644b75e347e962
Jon Sherrard <jon@andthats.it>
2017-03-10 20:57:39 +0000

    figured out how to test intermediate redirect headers

--- test/tests/redirects.test.js
+++ test/tests/redirects.test.js
@@ -19,12 +19,15 @@ describe('Handling redirects', () => {
   })
   after(() => tapestry.server.stop())
 
-  it('Redirect should return 301 status', (done) => {
-    // Can't work out how to get the intermediate 30x reply status
-    done()
+  // http://stackoverflow.com/questions/42136829/whats-difference-between-http-301-and-308-status-codes
+  it('Redirect should return 308 status', (done) => {
+    tapestry.server.inject(tapestry.server.info.uri + '/redirect/from/this-path', (res) => {
+          expect(res.statusCode).to.equal(308)
+          done()
+        })
   })
 
-  it('Redirect should to about/home with corrent content', (done) => {
+  it('Redirect should end up at about/home with correct content', (done) => {
     request
       .get(tapestry.server.info.uri + '/redirect/from/this-path', (err, res, body) => {
           expect(body).to.contain('dog named Jack')
