2e5e842e2d8336404f7e1c3ee043a4ac39a6267d
Jon Sherrard <jon@andthats.it>
2017-03-19 14:33:13 +0000

    display name for hoc

--- src/fetch-data.js
+++ src/fetch-data.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react'
 import AsyncProps from 'async-props'
+import fetchRouteData from './shared/fetch-route-data'
 
-export default  function(TopLevelComponent ) {
-  const name = TopLevelComponent.constructor.name
-  class extends Component {
+export default  function(TopLevelComponent) {
+  return class DataHOC extends Component {
     static loadProps({ params, loadContext }, cb) {
       // Don't need any IDs or anything we just use the name of the component `Post, Category, FrontPage` etc
-      return fetchRouteData(name, { params, loadContext, cb })
+      return fetchRouteData(TopLevelComponent.constructor.name, { params, loadContext, cb })
     }
 
     render() {
       return <TopLevelComponent {...this.props} />
     }
   }
-
-  Component.displayName = `Loader: ${name}`
-
-  return Component
 }
