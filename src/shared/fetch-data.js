72c688fcafc33b3160154f4a389f14805eccf6a6
Jon Sherrard <jon@andthats.it>
2017-03-21 16:48:36 +0000

    add data re-mapping

--- src/shared/fetch-data.js
+++ src/shared/fetch-data.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import AsyncProps from 'async-props'
 import has from 'lodash/has'
+import toArray from 'lodash/toArray'
 import isEmpty from 'lodash/isEmpty'
 import fetchRouteData from './fetch-route-data'
 import MissingView from './missing-view'
@@ -21,6 +22,11 @@ const fetchData = (TopLevelComponent, endpoint) => {
     render() {
       // check data exists and isnt a server errored response
       if (!this.props.data || isEmpty(this.props.data) || has(this.props.data, 'data.status')) return <MissingView />
+      const resp = this.props.data
+      let data = ('0' in resp) || resp instanceof Array ? { posts: toArray(resp) } : resp
+      if (data.posts.length == 1)  {
+        data = data.posts[0]
+      }
       // otherwise return the actual component
       return <TopLevelComponent {...this.props.data} />
     }
