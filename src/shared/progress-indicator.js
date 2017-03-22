d442687d6fec99bbc24c0e02c956c3d6f1bddbd4
Jon Sherrard <jon@andthats.it>
2017-03-22 16:51:00 +0000

    Render progress indicator

--- src/shared/progress-indicator.js
+++ src/shared/progress-indicator.js
@@ -1,7 +1,17 @@
 import React  from 'react'
 import Progress from 'react-progress'
 
-const ProgressIndicator = () =>
-  <Progress percent={60} stlye={{zIndex: 999999999}} />
+const ProgressIndicator = (props) =>
+  <div>
+    <Progress percent={60} stlye={{zIndex: 999999999}} />
+    {props.children}
+  </div>
+
+ProgressIndicator.propTypes = {
+  children: React.PropTypes.oneOfType([
+    React.PropTypes.arrayOf(React.PropTypes.node),
+    React.PropTypes.node
+  ])
+}
 
 export default ProgressIndicator
