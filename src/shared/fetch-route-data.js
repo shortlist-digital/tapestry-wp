3e2d1ca3fbd1b420d2cf8e52506380463320915b
Jon Sherrard <jon@andthats.it>
2017-03-24 11:41:55 +0000

    First go at the progress indicator

--- src/shared/fetch-route-data.js
+++ src/shared/fetch-route-data.js
@@ -1,12 +1,19 @@
 import axios from 'axios'
+import mitt from 'mitt'
 
-export default ({ endpoint, loadContext, cb }) => {
+let emitter = mitt()
+
+export default ({ endpoint, loadContext, cb, }) => {
+
   // create API path
   const baseUrl = `${loadContext.serverUri || window.location.origin}/api/v1`
   // go and fetch data from constructed API path
   // run callback from AsyncProps
+  if (typeof window !== 'undefined') {
+    window.tapestryEmitter.emit('dataStart', 'start')
+  }
   return axios.get(`${baseUrl}/${endpoint}`, {
-    onDownloadProgress: (event) => console.log(event) // eslint-disable-line no-console
+    onDownloadProgress: (event) => emitter.emit('dataProgress', event)
   })
     .then(resp => {
       // catch server error
@@ -14,7 +21,15 @@ export default ({ endpoint, loadContext, cb }) => {
       return resp.data
     })
     .then(resp => {
+      if (typeof window !== 'undefined') {
+        window.tapestryEmitter.emit('dataStop', 'stop')
+      }
       cb(null, { data: resp })
     })
-    .catch(error => cb(error))
+    .catch(error => {
+      if (typeof window !== 'undefined') {
+        window.tapestryEmitter.emit('dataStop', 'stop')
+      }
+      cb(error)
+    })
 }
--- src/shared/progress-indicator.js
+++ src/shared/progress-indicator.js
@@ -1,12 +1,49 @@
-import React  from 'react'
+import React, { Component } from 'react'
 import Progress from 'react-progress'
 
-const ProgressIndicator = (props) =>
-  <div>
-    <Progress percent={60} style={{zIndex: 999999999}} />
-    {/* props.children is essentially the <Router /> component */}
-    {props.children || <h1>No Routes defined!</h1>}
-  </div>
+class ProgressIndicator extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      percent: 0
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.interval = null
+  }
+
+  componentDidMount() {
+    window.tapestryEmitter.on('*', this.handleChange)
+  }
+
+  handleChange(type, event) {
+    if (type == 'dataStop') {
+      clearInterval(this.interval)
+      this.setState({percent: 100})
+      setTimeout(() => {
+        this.setState({percent: 0})
+      }, 1000)
+    }
+    if (type == 'dataStart') {
+      this.interval = setInterval(() => {
+        console.log('Ticking')
+        let updateState = this.state.percent + 10
+        this.setState({percent: updateState })
+      }, 500)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Progress percent={this.state.percent} height={2} speed={0.1} style={{zIndex: 999999999}} />
+        {/* props.children is essentially the <Router /> component */}
+        {this.props.children || <h1>No Routes defined!</h1>}
+      </div>
+    )
+  }
+}
+
 
 ProgressIndicator.propTypes = {
   children: React.PropTypes.oneOfType([
