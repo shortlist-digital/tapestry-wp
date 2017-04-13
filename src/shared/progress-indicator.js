c30c897f2c6ad81623a2765666786c4b835e0b7c
Jon Sherrard <jon@andthats.it>
2017-04-13 11:12:44 +0100

    Move dynamic CSS properties to inline styles to avod StyleSheet thrashing

--- src/shared/progress-indicator.js
+++ src/shared/progress-indicator.js
@@ -1,22 +1,12 @@
 import React, { Component, PropTypes } from 'react'
 import { css } from 'glamor'
 
-const style = ({ percent, complete }) => css({
+const style = css({
   backgroundColor: 'red',
   height: '2px',
   left: 0,
   position: 'fixed',
   top: 0,
-  opacity: complete ? 0 : 1,
-  transition: complete ?
-    `visibility 150ms linear 100ms,
-     opacity 150ms ease-out 100ms,
-     width 200ms ease-in-out` :
-    `visibility 150ms,
-     opacity 150ms ease-out,
-     width 150ms ease-in-out`,
-  width: `${percent}%`,
-  visibility: complete ? 'hidden' : 'visible',
   zIndex: 10000
 })
 
@@ -67,9 +57,24 @@ class ProgressIndicator extends Component {
   }
 
   render() {
+    const { complete, percent } = this.state
     return (
       <div>
-        <div className={style(this.state)} />
+        <div
+          className={style}
+          style={{
+            opacity: complete ? 0 : 1,
+            transition: complete ?
+              `visibility 150ms linear 100ms,
+              opacity 150ms ease-out 100ms,
+              width 200ms ease-in-out` :
+              `visibility 150ms,
+              opacity 150ms ease-out,
+              width 150ms ease-in-out`,
+            width: `${percent}%`,
+            visibility: complete ? 'hidden' : 'visible'
+          }}
+        />
         {this.props.children}
       </div>
     )
