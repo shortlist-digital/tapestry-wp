62785a7f78d038167b375615481f2dedd81afb2e
thomasdigby <thomasdigby1@gmail.com>
2017-06-01 14:14:45 +0100

    Reworked bin commands to improve reusability and user understanding? Also added support for custom babelrc

---
+++ src/utilities/create-compilers.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const fs = require('fs-extra')
+const webpack = require('webpack')
+
+const mergeConfigs = require('./merge-config')
+const configClientDefault = require('../webpack/client.config')
+const configServerDefault = require('../webpack/server.config')
+
+module.exports = ({ cwd, env }) => {
+
+  const configCustomPath = path.resolve(cwd, 'webpack.config.js')
+  const babelCustomPath = path.resolve(cwd, '.babelrc')
+
+  // fetch user webpack config
+  let configCustom = null
+  if (fs.existsSync(configCustomPath)) {
+    const module = require(configCustomPath)
+    configCustom = module.default || module
+  }
+  // fetch user babelrc
+  let babelCustom = null
+  if (fs.existsSync(babelCustomPath)) {
+    babelCustom = fs.readJsonSync(babelCustomPath)
+  }
+
+  const createCompiler = (configDefault) => {
+    // combine default/user webpack config
+    const webpackConfig = mergeConfigs({
+      configCustom,
+      configDefault,
+      options: { cwd, env, babelrc: babelCustom },
+      webpack
+    })
+    // kick off webpack compilation
+    return webpack(webpackConfig)
+  }
+
+  const serverCompiler = createCompiler(configServerDefault)
+  const clientCompiler = createCompiler(configClientDefault)
+
+  return {
+    serverCompiler,
+    clientCompiler
+  }
+}
