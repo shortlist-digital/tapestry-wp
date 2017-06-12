0e9d3f9b09d0268c4dbc50be0c98f2fe21e58dcc
thomasdigby <thomasdigby1@gmail.com>
2017-06-12 14:59:29 +0100

    Reconfigured preview/api handlers, added winston logs

--- src/server/handle-preview.js
+++ src/server/handle-preview.js
@@ -1,3 +1,5 @@
+import fetch from 'isomorphic-fetch'
+import winston from 'winston'
 import { errorObject } from '../utilities/logger'
 
 export default ({ server, config }) => {
@@ -6,10 +8,17 @@ export default ({ server, config }) => {
     method: 'GET',
     path: '/api/preview/v1/{query*}',
     handler: (req, reply) => {
-      const remote = `${config.siteUrl}/wp-json/revision/v1/${req.params.query}${req.url.search}`
+
+      const base = `${config.siteUrl}/wp-json/revision/v1`
+      const path = `${req.params.query}${req.url.search}`
+      const remote = `${base}/${path}`
+
+      winston.log('debug', `Server loading API response over HTTP for ${remote}`)
+
       fetch(remote)
         .then(resp => resp.json())
         .then(resp => {
+          winston.log('debug', `Server returned a fresh API response over HTTP for ${remote}`)
           reply(resp)
         })
         .catch(error => errorObject(error))
