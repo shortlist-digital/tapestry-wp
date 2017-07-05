7a64be58ed7871c1970a84891df75e357ee0da2f
thomasdigby <thomasdigby1@gmail.com>
2017-07-05 17:01:39 +0100

    Tweaked logs and improved consistency

--- src/server/handle-preview.js
+++ src/server/handle-preview.js
@@ -1,24 +1,24 @@
 import chalk from 'chalk'
 import fetch from 'isomorphic-fetch'
 import log from '../utilities/logger'
+import { stripLeadingTrailingSlashes } from '../utilities/cache-manager'
 
 export default ({ server, config }) => {
 
   server.route({
     method: 'GET',
     path: '/api/preview/v1/{query*}',
-    handler: (req, reply) => {
+    handler: (request, reply) => {
 
-      const base = `${config.siteUrl}/wp-json/revision/v1`
-      const path = `${req.params.query}${req.url.search}`
+      const base = `${stripLeadingTrailingSlashes(config.siteUrl)}/wp-json/revision/v1`
+      const path = `${request.params.query}${request.url.search}`
       const remote = `${base}/${path}`
-
-      log.debug(`Preview: Server loading API response over HTTP for ${chalk.green(remote)}`)
 
       fetch(remote)
         .then(resp => resp.json())
         .then(resp => {
-          log.debug(`Preview: Server returned a fresh API response over HTTP for ${chalk.green(remote)}`)
+
+          log.debug(`Preview API response via HTTP for ${chalk.green(remote)}`)
           reply(resp)
         })
         .catch(err => log.error(err))
