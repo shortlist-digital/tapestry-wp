5f5127cfd850d87be9dd6dccda7d20a5404f829f
Jon Sherrard <jon@andthats.it>
2017-07-17 15:23:11 +0100

    tidy up dev files

--- src/webpack/dev.config.js
+++ src/webpack/dev.config.js
@@ -1,8 +1,5 @@
 const path = require('path')
 const webpack = require('webpack')
-const CleanPlugin = require('clean-webpack-plugin')
-const AssetsPlugin = require('assets-webpack-plugin')
-const StatsPlugin = require('stats-webpack-plugin')
 const sharedModules = require('./shared')
 
 // exporting function to allow process.cwd() and environment to get passed through
@@ -40,81 +37,9 @@ module.exports = ({ cwd, env, babelrc }) => {
       // enable hot reload
       new webpack.HotModuleReplacementPlugin(),
       // expose environment to user
-      new webpack.DefinePlugin({ __DEV__ }),
+      new webpack.DefinePlugin({ __DEV__ })
       // output public path data for each bundle
-      new AssetsPlugin({
-        filename: 'assets.json',
-        path: path.resolve(cwd, '.tapestry'),
-        prettyPrint: true
-      }),
-      // remove output directory before saving new bundle
-      new CleanPlugin(['_scripts'], {
-        root: cwd,
-        verbose: false
-      })
     ]
-  }
-
-  // development specific config
-  if (env === 'development') {
-    // config.plugins already defined so lets push any extras
-    config.plugins.push(
-      // output chunk stats (path is relative to output path)
-      new StatsPlugin('../.tapestry/stats.json', {
-        chunkModules: true
-      })
-    )
-  }
-
-  // production specific config
-  if (env === 'production') {
-    // disable sourcemap
-    config.devtool = false
-    // expose chunkhash naming for long-life caching
-    config.output.filename = '[name].[chunkhash].js'
-    // non-changing vendor packages to combine in a vendor bundle
-    // react-helmet/glamor aren't required by Tapestry on the client, but they're very likely be put to use by the user so including it in the vendor file
-    config.entry.vendor = [
-      'async-props',
-      'glamor',
-      'react-dom',
-      'react-helmet',
-      'react-router',
-      'react'
-    ]
-    // config.plugins already defined so lets push any extras
-    config.plugins.push(
-      // production flag for React/others to minify correctly
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('production')
-      }),
-      // output common chunks into a vendor bundle
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'vendor',
-        minChunks: Infinity,
-        filename: 'vendor.[chunkhash].js'
-      }),
-      // https://webpack.js.org/plugins/loader-options-plugin/
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      }),
-      // minify/optimize output bundle, screw_ie8 a bunch
-      new webpack.optimize.UglifyJsPlugin({
-        comments: false,
-        compress: {
-          screw_ie8: true,
-          warnings: false
-        },
-        mangle: {
-          screw_ie8: true
-        },
-        output: {
-          comments: false,
-          screw_ie8: true
-        }
-      })
-    )
   }
 
   return config
