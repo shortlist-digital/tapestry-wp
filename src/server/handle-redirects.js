b8465438039033ea76a3076676c9065bb7a0456b
thomasdigby <thomasdigby1@gmail.com>
2017-07-20 15:21:57 +0100

    Removed weird spacing

--- src/server/handle-redirects.js
+++ src/server/handle-redirects.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-const setRedirects = (server, redirects ) => {
+const setRedirects = (server, redirects) => {
   Object
     .keys(redirects)
     .forEach(fromPath => {
