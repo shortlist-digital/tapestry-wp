9ba43edea8aea4b366e9974376e4d1df9e675eb2
Jon Sherrard <jon@andthats.it>
2017-07-25 18:20:29 +0100

    fix double ampersand

--- src/client/root.js
+++ src/client/root.js
@@ -12,7 +12,7 @@ if (module.hot) {
     let components = []
     function grabComponents(element) {
       // This only works for JSX routes, adjust accordingly for plain JS config
-      if (element.props & element.props.component) {
+      if (element.props && element.props.component) {
         components.push(element.props.component)
       }
       if (element.props && element.props.children) {
