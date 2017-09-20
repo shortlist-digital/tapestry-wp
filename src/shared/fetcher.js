4fdcb3d899f7f90a9670c5cdd65638334efb2a01
Jon Sherrard <jon@andthats.it>
2017-09-20 12:06:47 +0100

    use default timeout and increase keepAlive timeout

--- src/shared/fetcher.js
+++ src/shared/fetcher.js
@@ -3,12 +3,13 @@ import https from 'https'
 import fetch from 'isomorphic-fetch'
 import startsWith from 'lodash.startswith'
 
-
 const fetcher = (url) => {
   const Agent = startsWith(url, 'https') ? https.Agent : http.Agent
   const fetchOptions = {
-    agent: new Agent({ keepAlive: true }),
-    timeout: 5000
+    agent: new Agent({
+      keepAlive: true,
+      keepAliveMsecs: 20000
+    })
   }
   return fetch(url, fetchOptions)
 }
