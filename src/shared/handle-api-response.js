54ef01ed354ad7b6b9bf523bbc7db72abda0e57d
thomasdigby <thomasdigby1@gmail.com>
2017-12-13 10:48:23 +0000

    Restructure API handling for multi-endpoint

--- src/shared/handle-api-response.js
+++ src/shared/handle-api-response.js
@@ -2,33 +2,29 @@ import idx from 'idx'
 import HTTPStatus from 'http-status'
 import isEmpty from 'lodash.isempty'
 
-export default (response, route) => {
-  // WP returns single objects or arrays
-  const arrayResp = Array.isArray(response)
-  const routes = idx(route, _ => _.config.routes)
-  let routeConfig = routes ? routes.filter(item => item.path === route.path) : null
+const hasFailed = ({ response, config }) => (
   // 1: is it a falsey value
+  !response ||
   // 2: does it contain a status code? then it'll be an error response from WP
+  idx(response, _ => _.data.status) ||
   // 3: is it an array, that is empty, and options.allowEmptyResponse is falsey
-  if (
-    !response ||
-    idx(response, _ => _.data.status) ||
-    (
-      arrayResp &&
-      isEmpty(response) &&
-      !idx(routeConfig, _ => _[0].options.allowEmptyResponse)
-    )
-  ) {
+  (
+    Array.isArray(response) &&
+    isEmpty(response) &&
+    !idx(config, _ => _[0].options.allowEmptyResponse)
+  )
+)
+
+export default (response, route) => {
+
+  const routes = idx(route, _ => _.config.routes)
+  const config = routes
+    ? routes.filter(item => item.path === route.path)
+    : {}
+
+  if (hasFailed({ response, config })) {
     // Assign status
     let status = idx(response, _ => _.data.status) || HTTPStatus.NOT_FOUND
-
-    // Check for static routes with no endpoint
-    const routeExistsWithNoEndpoint = idx(routeConfig, _ => _[0].path) &&
-      !idx(routeConfig, _ => _[0].endpoint)
-
-    // If the route is registered but has no endpoint, assume 200
-    status = routeExistsWithNoEndpoint ? HTTPStatus.OK : status
-
     return {
       code: status,
       message: HTTPStatus[status]
@@ -36,5 +32,7 @@ export default (response, route) => {
   }
   // to avoid React mangling the array to {'0':{},'1':{}}
   // wrap in an object
-  return arrayResp ? { data: response } : response
+  return Array.isArray(response)
+    ? { data: response }
+    : response
 }
