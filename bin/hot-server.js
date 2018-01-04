961f7e5f1fbac76438649b6c7c6496ac16f6a373
thomasdigby <thomasdigby1@gmail.com>
2018-01-04 10:36:24 +0000

    tidy up

--- bin/hot-server.js
+++ bin/hot-server.js
@@ -1,9 +1,7 @@
 const path = require('path')
 const cwd = process.cwd()
-const env = 'development'
 // server path relative to root
 const serverPath = path.resolve(cwd, '.tapestry/server.js')
 // require server and boot
 const server = require(serverPath).default
-server({ cwd, env })
-// })
+server({ cwd, env: 'development' })
